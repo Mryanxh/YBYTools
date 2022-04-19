679bfcc9be3eb76633979d2791e5b1b9c7f4468f
Yanxh <mr.yanxh@foxmail.com>
2022-04-19 12:33:15 +0800

    1

--- ReleaseMe/Program.cs
+++ ReleaseMe/Program.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Data;
 
 namespace ReleaseMe
 {
     internal class Program
     {
         //忽略的文件类型
-        static string[] IgnoreFile = new string[] {".pdb", ".xml" };
-        
+        static string[] IgnoreFile = new string[] { ".pdb", ".xml" };
+
         static void Main(string[] args)
         {
             try
@@ -21,7 +23,7 @@ namespace ReleaseMe
                 string path = Directory.GetCurrentDirectory();
                 List<string> files = Directory.GetFiles(path).ToList();
                 List<string> UpdateFiles = new List<string>();
-                //生成MD数据
+                //生成MD5数据
                 foreach (string file in files)
                 {
                     if (IgnoreFile.Contains(Path.GetExtension(file)))
@@ -43,6 +45,10 @@ namespace ReleaseMe
                         MD5 = GetMD5WithFilePath(file),
                     });
                 }
+
+                Console.Write("请输入版本号(例1.0.0.0):");
+                string VersionUpdateStr = Console.ReadLine();
+
                 Console.Write("请输入更新日志:");
                 string VersionUpdateInfo = Console.ReadLine();
 
@@ -52,12 +58,48 @@ namespace ReleaseMe
                     MustUpdate = true,
                     Updatetime = DateTime.Now,
                     UpdateInfo = VersionUpdateInfo,
-                    VersionStr = "1.0.0.1"
+                    VersionStr = VersionUpdateStr
                 };
 
                 //保存成本地文件
                 string newVersionStr = JsonConvert.SerializeObject(newVersionInfo, Formatting.Indented);
-                File.WriteAllText($"{path}\\Release.info", newVersionStr, Encoding.UTF8);
+                string releasefilepath = $"{path}\\Release.info";
+                FileStream fs;
+                if (File.Exists(releasefilepath))
+                {
+                    fs = File.Open(releasefilepath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                }
+                else
+                {
+                    fs = File.OpenWrite(releasefilepath);
+                }
+                try
+                {
+                    fs.Position = 0;
+                    if (fs.CanWrite)
+                    {
+                        byte[] newVersionByte = Encoding.Default.GetBytes(newVersionStr);
+                        fs.Write(newVersionByte, 0, newVersionByte.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine("无法写入文件");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    fs.Flush();
+                    fs.Close();
+                    fs.Dispose();
+                }
+                //File.WriteAllText(releasefilepath, newVersionStr, Encoding.UTF8);
+
+                //下载完成后启动指定的Exe文件,并退出当前窗口
+                Process.Start($"{path}\\");
 
                 Console.WriteLine("文件生成成功,请及时上传至服务器！");
             }
