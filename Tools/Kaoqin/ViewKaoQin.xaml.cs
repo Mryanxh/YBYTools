4d1a097c058bc46ce04484d055934f73369eb48d
Yanxh <mr.yanxh@foxmail.com>
2022-06-02 16:34:11 +0800

    1

--- Tools/Kaoqin/ViewKaoQin.xaml.cs
+++ Tools/Kaoqin/ViewKaoQin.xaml.cs
@@ -136,23 +136,5 @@ namespace AlphaYanTools.Kaoqin
                 return string.Empty;
             }
         }
-
-        string HttpGet(string Url)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-            request.Method = "GET";
-            request.ContentType = "text/html;charset=UTF-8";
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-            string retString = myStreamReader.ReadToEnd();
-            myStreamReader.Close();
-            myResponseStream.Close();
-
-            return retString;
-        }
-
-
     }
 }
